cf1ba0126f4cf8c4c05fc1834b6cc207dfd632c4
BinaryKiwiz <45517110+BinaryKiwiz@users.noreply.github.com>
2020-03-16 20:18:15 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
+console.log("Loading Variables");
+var Info = "You are a quark in an infinite universe";
+Quarks = 1;
+ReplicationIncrease = 1;
 
+console.log("Loading Functions");
+function Replicate(){
+  Quarks += ReplicationIncrease;
+}
